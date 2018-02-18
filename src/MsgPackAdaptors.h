f17f2d1b06a1248462dfcb83ebb1fa9342d95cc9
Thomas Kolb <cfr34k-git@tkolb.de>
2018-02-18 18:12:21 +0100

    Moved MessagePack adaptors to separate file

---
+++ src/MsgPackAdaptors.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <msgpack.hpp>
+
+#include "Field.h"
+#include "Bot.h"
+
+/*!
+ * \file
+ * \brief MessagePack adaptors for game objects.
+ */
+
+namespace msgpack {
+MSGPACK_API_VERSION_NAMESPACE(MSGPACK_DEFAULT_API_NS) {
+namespace adaptor {
+
+	template <> struct pack< std::shared_ptr<Food> >
+	{
+		template <typename Stream> msgpack::packer<Stream>& operator()(msgpack::packer<Stream>& o, std::shared_ptr<Food> const& v) const
+		{
+			o.pack(v->getGUID());
+
+			const Vector &pos = v->getPosition();
+			o.pack(pos.x());
+			o.pack(pos.y());
+
+			o.pack(v->getValue());
+
+			return o;
+		}
+	};
+
+	template <> struct pack< std::shared_ptr<Bot> >
+	{
+		template <typename Stream> msgpack::packer<Stream>& operator()(msgpack::packer<Stream>& o, std::shared_ptr<Bot> const& v) const
+		{
+			o.pack(v->getGUID());
+
+			o.pack(std::string("unnamed")); // FIXME: pack bot name here
+
+			o.pack(1.0); // FIXME: segment radius
+
+			// segments
+			const Snake::SegmentList &segments = v->getSnake()->getSegments();
+			o.pack_array(segments.size());
+			for(auto &s: segments) {
+				o.pack(s->pos.x());
+				o.pack(s->pos.y());
+			}
+
+			// FIXME: colormap: array of RGB values
+			o.pack_array(3);
+			o.pack(0xFF0000);
+			o.pack(0x00FF00);
+			o.pack(0x0000FF);
+
+			return o;
+		}
+	};
+
+} // namespace adaptor
+} // MSGPACK_API_VERSION_NAMESPACE(MSGPACK_DEFAULT_API_NS)
+} // namespace msgpack
