13de99ce73b99fbc81553e1965a3cc08d3120ac6
Hubert Denkmair <hubert@denkmair.de>
2018-04-22 12:32:32 +0200

    add database interface

---
+++ src/Database.cpp
@@ -0,0 +1,66 @@
+#include "Database.h"
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+
+using namespace db;
+
+void MysqlDatabase::Connect(std::string host, std::string username, std::string password, std::string database)
+{
+	_driver = get_driver_instance();
+	_connection = std::unique_ptr<sql::Connection>(_driver->connect(host, username, password));
+	_connection->setSchema(database);
+
+	_getBotScriptStmt = std::unique_ptr<sql::PreparedStatement>(
+		_connection->prepareStatement(
+			"SELECT "
+			"	s.id AS snake_id, "
+			"	s.name AS snake_name, "
+			"	sv.id AS version_id, "
+			"	sv.code AS code "
+			"FROM core_snakeversion sv "
+			"LEFT JOIN core_snake s ON (sv.snake_id=s.id) "
+			"WHERE sv.id=(SELECT MAX(id) FROM core_snakeversion WHERE snake_id=?);"
+		)
+	);
+	_getAllBotScriptsStmt = std::unique_ptr<sql::PreparedStatement>(
+		_connection->prepareStatement(
+			"SELECT "
+			"	s.id AS snake_id, "
+			"	s.name AS snake_name, "
+			"	sv.id AS version_id, "
+			"	sv.code AS code "
+			"FROM core_snakeversion sv "
+			"LEFT JOIN core_snake s ON (sv.snake_id=s.id) "
+			"WHERE sv.id=(SELECT MAX(id) FROM core_snakeversion WHERE snake_id=s.id);"
+		)
+	);
+}
+
+std::vector<BotScript> MysqlDatabase::GetBotScript(int bot_id)
+{
+	_getBotScriptStmt->setInt(1, bot_id);
+	return GetScripts(_getBotScriptStmt.get());
+}
+
+std::vector<BotScript> MysqlDatabase::GetBotScripts()
+{
+	return GetScripts(_getAllBotScriptsStmt.get());
+}
+
+std::vector<BotScript> MysqlDatabase::GetScripts(sql::PreparedStatement *stmt)
+{
+	std::unique_ptr<sql::ResultSet> res(stmt->executeQuery());
+	std::vector<BotScript> retval;
+	while (res->next())
+	{
+		retval.emplace_back(
+			res->getInt(IDX_BOTSCRIPT_BOT_ID),
+			res->getString(IDX_BOTSCRIPT_BOT_NAME),
+			res->getInt(IDX_BOTSCRIPT_VERSION_ID),
+			res->getString(IDX_BOTSCRIPT_CODE)
+		);
+	}
+	return retval;
+}
+
---
+++ src/Database.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <memory>
+#include <mysql_connection.h>
+#include <cppconn/driver.h>
+#include <cppconn/prepared_statement.h>
+
+namespace db
+{
+	class BotScript
+	{
+		public:
+			int bot_id = -1;
+			int version_id = -1;
+			std::string bot_name;
+			std::string code;
+
+			BotScript(int aBotId, std::string aBotName, int aVersionId, std::string aCode)
+				: bot_id(aBotId), version_id(aVersionId)
+				, bot_name(aBotName), code(aCode)
+			{}
+	};
+
+	class IDatabase
+	{
+		public:
+			virtual ~IDatabase() = default;
+			virtual std::vector<BotScript> GetBotScript(int bot_id) = 0;
+			virtual std::vector<BotScript> GetBotScripts() = 0;
+	};
+
+	class MysqlDatabase : public IDatabase
+	{
+		public:
+			void Connect(std::string host, std::string username, std::string password, std::string database);
+			std::vector<BotScript> GetBotScript(int bot_id) override;
+			std::vector<BotScript> GetBotScripts() override;
+
+		private:
+			enum {
+				IDX_BOTSCRIPT_BOT_ID = 1,
+				IDX_BOTSCRIPT_BOT_NAME = 2,
+				IDX_BOTSCRIPT_VERSION_ID = 3,
+				IDX_BOTSCRIPT_CODE = 4
+			};
+
+			sql::Driver *_driver = nullptr;
+			std::unique_ptr<sql::Connection> _connection;
+			std::unique_ptr<sql::PreparedStatement> _getBotScriptStmt;
+			std::unique_ptr<sql::PreparedStatement> _getAllBotScriptsStmt;
+
+			std::vector<BotScript> GetScripts(sql::PreparedStatement *stmt);
+	};
+}
