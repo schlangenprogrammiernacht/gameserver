e2349c2be12a381d0897724e7a67c099971e1416
Thomas Kolb <thomas@tkolb.de>
2018-05-10 21:49:44 +0200

    More debugging output

--- src/Field.cpp
+++ src/Field.cpp
@@ -92,7 +92,7 @@ std::shared_ptr<Bot> Field::newBot(std::unique_ptr<db::BotScript> data, std::str
 	initErrorMessage = "";
 	if (bot->init(initErrorMessage))
 	{
-		std::cerr << "Created Bot with ID " << bot->getGUID() << std::endl;
+		std::cerr << "Created Bot with ID " << bot->getGUID() << ", DB-ID " << bot->getDatabaseId() << ", Name: " << bot->getName() << std::endl;
 		m_updateTracker->botLogMessage(bot->getViewerKey(), "starting bot");
 		m_updateTracker->botSpawned(bot);
 		m_bots.insert(bot);
