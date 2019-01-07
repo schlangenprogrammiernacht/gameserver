bc8dc8de0cea846c26783f43541700a7ea7ca0ec
Thomas Kolb <cfr34k-git@tkolb.de>
2019-01-07 21:20:39 +0100

    Docker: bind-mount individual shared memory for each bot

--- docker4bots/2_run_spn_cpp_bot.sh
+++ docker4bots/2_run_spn_cpp_bot.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-if [ -z "$1" ]; then
+SPN_SHM_HOSTDIR="/mnt/spn_shm"
+
+BOTNAME="$1"
+
+if [ -z "$BOTNAME" ]; then
 	echo "Argument required: bot name"
 	exit 1
 fi
@@ -8,4 +12,5 @@ fi
 docker run \
 	--memory=32M --memory-swap=32M --cpus=1 \
 	--tmpfs /run --tmpfs /tmp \
-	spn_cpp_bot:$1
+	-v $SPN_SHM_HOSTDIR/$BOTNAME:/spnshm \
+	spn_cpp_bot:$BOTNAME
