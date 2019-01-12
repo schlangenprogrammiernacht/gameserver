2096b95c1190662c4a67826d3273a35029de26e9
Thomas Kolb <thomas@tkolb.de>
2019-01-12 22:30:10 +0100

    Bot: define IPC format and set up shared memory

---
+++ docker4bots/spn_cpp_base/spn_cpp_framework/src/ipc_format.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <stdint.h>
+
+typedef float ipc_real_t;
+typedef uint64_t ipc_guid_t;
+
+struct IpcFoodInfo {
+	ipc_real_t x;      //!< Relative position X
+	ipc_real_t y;      //!< Relative position Y
+	ipc_real_t v;      //!< Food value
+	ipc_real_t d;      //!< Direction angle
+	ipc_real_t dist;   //!< Distance
+};
+
+struct IpcBotInfo {
+	ipc_guid_t bot_id;        //!< Bot ID
+	char       bot_name[64];  //!< Bot name (the beginning of it, at least)
+};
+
+struct IpcSegmentInfo {
+	ipc_real_t x;       //!< Relative position X
+	ipc_real_t y;       //!< Relative position Y
+	ipc_real_t r;       //!< Segment radius
+	ipc_real_t d;       //!< Direction angle
+	ipc_real_t dist;    //!< Distance
+	ipc_guid_t bot_id;  //!< Bot ID
+};
+
+struct IpcColor {
+	uint8_t r; //!< Red channel
+	uint8_t g; //!< Green channel
+	uint8_t b; //!< Blue channel
+};
+
+const size_t IPC_FOOD_MAX_BYTES = 1 * 1024*1024;
+const size_t IPC_FOOD_MAX_COUNT = IPC_FOOD_MAX_BYTES / sizeof(struct IpcFoodInfo);
+
+const size_t IPC_BOT_MAX_COUNT = 1024;
+const size_t IPC_BOT_MAX_BYTES = IPC_BOT_MAX_COUNT * sizeof(struct IpcBotInfo);
+
+const size_t IPC_SEGMENT_MAX_BYTES = 1 * 1024*1024;
+const size_t IPC_SEGMENT_MAX_COUNT = IPC_SEGMENT_MAX_BYTES / sizeof(struct IpcSegmentInfo);
+
+const size_t IPC_COLOR_MAX_COUNT = 1024;
+const size_t IPC_COLOR_MAX_BYTES = IPC_COLOR_MAX_COUNT * sizeof(struct IpcColor);
+
+/*!
+ * Shared memory structure.
+ *
+ * This structure represents the contents of the memory the bot shares with the
+ * gameserver.
+ */
+struct IpcSharedMemory {
+	uint32_t foodCount;
+	struct IpcFoodInfo foodInfo[IPC_FOOD_MAX_COUNT];
+
+	uint32_t botCount;
+	struct IpcBotInfo botInfo[IPC_BOT_MAX_COUNT];
+
+	uint32_t segmentCount;
+	struct IpcSegmentInfo segmentInfo[IPC_SEGMENT_MAX_COUNT];
+
+	uint32_t colorCount;
+	struct IpcColor colors[IPC_COLOR_MAX_COUNT];
+};
+
+const size_t IPC_SHARED_MEMORY_BYTES = sizeof(struct IpcSharedMemory);
--- docker4bots/spn_cpp_base/spn_cpp_framework/src/main.cpp
+++ docker4bots/spn_cpp_base/spn_cpp_framework/src/main.cpp
@@ -1,8 +1,63 @@
 #include <iostream>
+#include <cstring>
+
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+
+#include "ipc_format.h"
+
+struct IpcSharedMemory* setup_shm(int *fd)
+{
+	*fd = open("/spnshm/shm", O_RDWR);
+	if(*fd == -1) {
+		std::cerr << "shm_open() failed: " << strerror(errno) << std::endl;
+		return NULL;
+	}
+
+	void *shared_mem = mmap(NULL, IPC_SHARED_MEMORY_BYTES, PROT_READ, MAP_SHARED, *fd, 0);
+	if(shared_mem == (void*)-1) {
+		std::cerr << "mmap() failed: " << strerror(errno) << std::endl;
+		close(*fd);
+		return NULL;
+	}
+
+	return reinterpret_cast<struct IpcSharedMemory*>(shared_mem);
+}
+
+void shutdown_shm(int fd, void *shm)
+{
+	int ret = munmap(shm, IPC_SHARED_MEMORY_BYTES);
+	if(ret == -1) {
+		std::cerr << "munmap() failed: " << strerror(errno) << std::endl;
+	}
+
+	close(fd);
+}
 
 int main(int argc, char **argv)
 {
+	int shm_fd;
+	struct IpcSharedMemory *shm;
+
 	std::cerr << "Hello from the bot code!" << std::endl;
+
+	std::cerr << "Setting up shared memory..." << std::endl;
+
+	shm = setup_shm(&shm_fd);
+	if(!shm) {
+		std::cerr << "Failed to set up shared memory." << std::endl;
+		return 1;
+	}
+
+	std::cerr << "Set up shared memory at address 0x" << std::hex << shm << " with size of " << std::dec << IPC_SHARED_MEMORY_BYTES << " bytes." << std::endl;
+
+	std::cerr << "Shutting down shared memory..." << std::endl;
+
+	shutdown_shm(shm_fd, shm);
 
 	return 0;
 }
