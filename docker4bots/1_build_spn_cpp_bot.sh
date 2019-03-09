110bac81eefd7d510bb697825557ee6a24cac1ed
Thomas Kolb <thomas@tkolb.de>
2019-03-10 00:07:08 +0100

    Actually use user code when building docker containers

--- docker4bots/1_build_spn_cpp_bot.sh
+++ docker4bots/1_build_spn_cpp_bot.sh
@@ -1,8 +1,30 @@
 #!/bin/sh
 
 if [ -z "$1" ]; then
-	echo "Argument required: bot name"
+	echo "Argument required: snake version id"
 	exit 1
 fi
 
-docker build -t spn_cpp_bot:$1 spn_cpp_bot/
+if [ -z "$2" ]; then
+	echo "Argument required: path to user code file"
+	exit 1
+fi
+
+VERSION_ID=$1
+CODE_FILE=$2
+
+CONTEXTDIR=$(mktemp -d)
+
+cp spn_cpp_bot/Dockerfile "$CONTEXTDIR"
+cp "$CODE_FILE" "$CONTEXTDIR/usercode.cpp"
+
+pushd "$CONTEXTDIR"
+
+docker build -t spn_cpp_bot:$VERSION_ID .
+EXITCODE=$?
+
+popd
+
+rm -r "$CONTEXTDIR"
+
+exit $EXITCODE
