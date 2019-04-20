866fbafee175545a6e0d6c22cc3c97c399f625d3
Thomas Kolb <thomas@tkolb.de>
2019-04-21 01:25:04 +0200

    Doxygen: Added some content to the main page

---
+++ docker4bots/spn_cpp_base/spn_cpp_framework/src/doxygen_mainpage.h
@@ -0,0 +1,13 @@
+/*!
+ * \mainpage SPN Bot Framework
+ *
+ * \section Introduction
+ *
+ * Welcome to the SPN bot framework documentation.
+ *
+ * For a quick overview, we recommend to take a look at the Api class and move
+ * through the different IPC structures from there.
+ *
+ * This framework is a very thin abstraction layer between your code and the
+ * shared memory used for fast communication with the gameserver.
+ */
