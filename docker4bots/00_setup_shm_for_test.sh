ed8f999a5ca8c2fcd541bd6727e798f77b03a2cc
xieve <xieve@mailbox.org>
2019-06-06 22:15:03 +0200

    Removed `sudo` as a dependency

--- docker4bots/00_setup_shm_for_test.sh
+++ docker4bots/00_setup_shm_for_test.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -e
 
+if [ '$EUID' -ne 0 ]
+    then echo 'This script needs to be run as root.'
+    exit
+fi
+
 source config.sh
 
-sudo mount -t tmpfs -o noexec,size=1G none $SPN_SHM_HOSTDIR/
-sudo chown $(id -u):$(id -g) $SPN_SHM_HOSTDIR
+mkdir -p $SPN_SHM_HOSTDIR/
+mount -t tmpfs -o noexec,size=1G none $SPN_SHM_HOSTDIR/
+chown $(id -u):$(id -g) $SPN_SHM_HOSTDIR
 mkdir $SPN_SHM_HOSTDIR/testbot
-
