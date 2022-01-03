1f5c51f92e309de75cc7ebd306acf8f268dd9b96
Thomas Kolb <thomas@tkolb.de>
2022-01-03 12:24:50 +0100

    rust: added Dockerfile and wrapper script

---
+++ docker4bots/spn_rust_base/bot_wrapper.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -e
+
+action="$1"
+
+echo "Hello from the SPN Rust container!"
+
+case "$action" in
+	compile)
+		# copy the code in, build and move the binary out
+		cd /spnbot/spn_rust_framework/
+
+		cp -f /spndata/usercode.rs src/usercode.rs
+		./build.sh && cp target/debug/spn_rust_bot /spndata/bot
+		;;
+
+	run)
+		# run the bot and allow coredumps
+		ulimit -c unlimited
+
+		cd /spndata/
+		exec ./bot
+		;;
+
+	*)
+		echo "Invalid action: $action"
+		exit 1
+esac
